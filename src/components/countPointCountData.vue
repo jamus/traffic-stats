a1902561f22bab5f79b9db126c3ff54602abca29
Jamie Reynolds <jamus@jamus.co.uk>
2019-10-17 22:36:21 +0100

    Added year labels

--- src/components/countPointCountData.vue
+++ src/components/countPointCountData.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     countPointCountData {{ countPointID }}
-    <div v-if="CountPointCountsData">
+    <div v-if="CountPointCountsData && yearsCounted">
+      {{ yearsCounted }}
       <!-- {{ this.getCountsByDirection('E') }} -->
       <!-- {{ chartData }} -->
       <countPointCountChart :chartData="chartData" :options="options" />
@@ -16,7 +17,7 @@ import { mapState, mapGetters } from 'vuex';
 import countPointCountChart from '@/components/countPointCountChart';
 const cheese = {
             label: 'cheese',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3, 3, 12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -29,7 +30,7 @@ const cheese = {
         };
 const bacon = {
             label: 'bacon',
-            data: [3, 3, 5, 6, 8, 9],
+            data: [3, 3, 5, 6, 8, 9, 5, 2, 3, 12, 19, 3, 5, 2, 3, 3, 12, 19, 21],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -53,10 +54,6 @@ export default {
   },
   data() {
     return {
-      chartData: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [cheese, bacon]
-      },
       options: {
         lineTension: 0
       }
@@ -67,7 +64,7 @@ export default {
       .catch(error => {
         this.$store.commit('countPointCounts/setIsRequestingCountPointData', false);
         this.$store.dispatch('countPointCounts/clearPropertyDetailsData');
-        console.error('error requestCountPointCountsData');
+        console.error('error requestCountPointCountsData', error);
       });
   },
   computed: {
@@ -77,7 +74,28 @@ export default {
     ]),
     ...mapGetters('countPointCounts', [
       'getCountsByDirection'
-    ])
+    ]),
+    chartData() {
+      return {
+        labels: this.yearsCounted,
+        datasets: [cheese, bacon]
+      }
+    },
+    yearsCounted() {
+      return [...new Set(this.CountPointCountsData.map(item => item.year))]; // ES6 has a native object Set to store unique value
+    },
+    countedNorth() {
+      return this.getCountsByDirection('N');
+    },
+    countedSouth() {
+      return this.getCountsByDirection('S');
+    },
+    countedEast() {
+      return this.getCountsByDirection('E');
+    },
+    countedWest() {
+      return this.getCountsByDirection('W');
+    }
   }
 };
 </script>
