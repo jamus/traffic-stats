a45896776fb487f25167deff6879865b2ea62017
Jamie Reynolds <jamus@jamus.co.uk>
2019-10-20 21:43:44 +0100

    added option radio style

--- src/components/theListFilter.vue
+++ src/components/theListFilter.vue
@@ -2,11 +2,11 @@
   <div>
     <div class="mx-auto w-1/5">
       <div class="flex items-center justify-center">
-        <input type="radio" id="length" value="length" v-model="filterby" class="toggle-left">
+        <input type="radio" id="length" value="length" v-model="filterby" class="toggle toggle-left">
         <label for="length" class="btn">Length</label>
-        <input type="radio" id="type" value="type" v-model="filterby" class="toggle-centre">
+        <input type="radio" id="type" value="type" v-model="filterby" class="toggle toggle-centre">
         <label for="type" class="btn">Type</label>
-        <input type="radio" id="category" value="category" v-model="filterby" class="toggle-right">
+        <input type="radio" id="category" value="category" v-model="filterby" class="toggle toggle-right">
         <label for="category" class="btn">category</label>
       </div>
     </div>
@@ -17,13 +17,23 @@
       </span>
       <span class="text-sm p-4">20 km</span>
     </div>
-    <div v-if="filterby === 'type'" class="option">
-      <select v-model="type">
-        <option disabled value="">Road Type</option>
-        <option value="major">Major</option>
-        <option value="minor">Minor</option>
-      </select>
-    </div>
+    <ul v-if="filterby === 'type'" class="option">
+      <li>
+        <input type="radio" id="all" value="" v-model="type" >
+        <label for="all" >both</label>
+        <div class="check"></div>
+      </li>
+      <li>
+        <input type="radio" name="minor" id="minor" value="minor" v-model="type" >
+        <label for="minor" >minor</label>
+        <div class="check"></div>
+      </li>
+      <li>
+        <input type="radio" id="major" value="major" v-model="type" >
+        <label for="major" >major</label>
+        <div class="check"></div>
+      </li>
+    </ul>
     <div v-if="filterby === 'category'" class="option">
       <select v-model="category">
         <option disabled value="">Road category</option>
@@ -130,7 +140,7 @@ input[type=range]::-webkit-slider-thumb {
   @apply text-yellow-600 border-2 border-yellow-700 inline-block px-6 py-2 relative text-center;
 }
 
-input[type="radio"] {
+input[type="radio"].toggle {
     display: none;
     & + label{
         text-transform: capitalize;
@@ -159,4 +169,77 @@ input[type="radio"] {
 .option {
   @apply h-12
 }
+
+ul.option {
+  @apply pl-4 pt-2 inline-block text-left;
+
+  input[type=radio] {
+    top: 0;
+    position: absolute;
+    visibility: hidden;
+  }
+  label {
+    text-transform: capitalize;
+    display: inline-block;
+    position: relative;
+    margin: 10px auto;
+    height: 30px;
+    z-index: 9;
+    cursor: pointer;
+    width: 100%;
+    padding: 0 0 0 45px;
+    @apply text-yellow-600;
+  }
+
+  li {
+    position: relative;
+    text-align: left;
+    display: inline-block;
+    width: 130px;
+  }
+  
+  li:hover label {
+    @apply text-yellow-500;
+  }
+
+  li .check {
+  display: inline-block;
+  position: absolute;
+  border-radius: 100%;
+  height: 25px;
+  width: 25px;
+  top: 10px;
+  left: 0;
+  @apply border border-2 border-yellow-600  bg-yellow-700;
+}
+
+  li:hover .check {
+    @apply border border-2 border-yellow-500 bg-yellow-700;
+  }
+
+  li .check::before {
+    display: block;
+    position: absolute;
+    content: '';
+    border-radius: 100%;
+    height: 15px;
+    width: 15px;
+    top: 3px;
+    left: 3px;
+    margin: auto;
+  }
+
+  input[type=radio]:checked ~ .check {
+    // @apply border border-4 border-yellow-500;
+  }
+
+  input[type=radio]:checked ~ .check::before{
+    @apply bg-yellow-500;
+  }
+
+  input[type=radio]:checked ~ label{
+    @apply text-yellow-500;
+  }
+}
+
 </style>
